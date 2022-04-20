a90e4d46b91c25688422528e863b6e0289eefecb
umasumathi57 <umasumathi57@gmail.com>
2022-04-20 13:33:58 +0530

    POC2

---
+++ task-front/src/App.js
@@ -0,0 +1,10 @@
+const App=()=>
+{
+  return
+  (
+    <>
+    <h3>Welcome to POC2 React js</h3>
+    </>
+  )
+}
+export default App;
---
+++ task-front/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'; //New update
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <>
+    <App />
+  </>
+);
+
+npm a
